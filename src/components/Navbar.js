27a2fa8f4879f3025bee9d06dda589dbebcb54a3
ahsanshah <shahahsan56@gmail.com>
2020-08-30 23:01:34 +0500

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,24 @@ import '../App.css';
 const Navbar = () => {
     return ( 
         <div>
-        <nav class="navbar navbar-expand-lg navbar-primary">
+        <nav class="navbar navbar-expand-lg">
             <span class="navbar-brand">CodeGenie</span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon bg-dark-gray"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <span class="nav-link">Home <span class="sr-only">(current)</span></span>
+                <li class="nav-item">
+                    <span class="nav-link">Home </span>
                 </li>
                 <li class="nav-item">
-                    <span class="nav-link" href="#">Features</span>
+                    <span class="nav-link" href="#">About us</span>
                 </li>
                 <li class="nav-item">
-                    <span class="nav-link" href="#">Pricing</span>
+                    <span class="nav-link" href="#">Services</span>
                 </li>
                 <li class="nav-item">
-                    <span class="nav-link my-2 my-sm-0" href="#">Disabled</span>
+                    <span class="nav-link my-2 my-sm-0" href="#">Career</span>
                 </li>
                 </ul>
             </div>
