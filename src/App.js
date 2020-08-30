0480c5f80dd5b63931bece731212ea1818a1fae1
ahsanshah <shahahsan56@gmail.com>
2020-08-30 23:21:13 +0500

    Added Text

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import Navbar from './components/Navbar';
+import TopComponent from './components/TopComponent';
+
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <TopComponent />
     </div>
   );
 }
