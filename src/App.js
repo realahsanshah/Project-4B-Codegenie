b847f1ee3688dbad745a266ec7b5788dc25d3a58
ahsanshah <shahahsan56@gmail.com>
2020-08-30 22:50:04 +0500

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import Navbar from './components/Navbar';
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import '../App.css';
 
 const Navbar = () => {
     return ( 
         <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-primary">
             <span class="navbar-brand">CodeGenie</span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
